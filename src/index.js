d27013c9cd5059841b345534573d04f65195cff5
Sami Khan <sami999khan999@gmail.com>
2023-09-14 01:41:36 +0600

    done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// //  React Router
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// // React Router is a popular library for handling routing and navigation in React applications. It provides a way to manage the URLs (Uniform Resource Locators) of your application and render different components based on the current URL, allowing you to create multi-page applications or single-page applications (SPAs) with distinct views. //
+
+//============================================================================================================================================//
+
+// // Routes - In React Router, routes are used to define how different components or views should be rendered based on the URL or path in your application. React Router is a library for handling routing and navigation in React applications. Routes determine what content to display when a specific URL is accessed. //
+
+//--------------------------------------------------------------------------------------------------------------------------------------------//
+
+// // more ditails in ./src/App.js and ./src/index.js
+
+//============================================================================================================================================//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//============================================================================================================================================//
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
